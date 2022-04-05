fd6ac3f8640e8dfaacf4257dd94924900d7be6fc
Martin Parm <parmus@gmail.com>
2022-04-05 22:54:15 +0200

    Add custom editors

---
+++ Editor/LocationColdStartInitializerEditor.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Games.GrumpyBear.LevelManagement.Editor
+{
+    
+    [CustomEditor(typeof(LocationColdStartInitializer))]
+    public class LocationColdStartInitializerEditor: UnityEditor.Editor
+    {
+        private SerializedProperty _locationManagerProperty;
+        private SerializedProperty _locationProperty;
+
+        private void OnEnable()
+        {
+            _locationManagerProperty = serializedObject.FindProperty("_locationManager");
+            _locationProperty = serializedObject.FindProperty("_location");
+        }
+
+        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            var locationManager = _locationManagerProperty.objectReferenceValue as LocationManager;
+            var location = _locationProperty.objectReferenceValue as Location;
+            
+            if (locationManager == null) EditorGUILayout.HelpBox("Location Manager missing", MessageType.Warning);
+            if (location == null) EditorGUILayout.HelpBox("Location missing", MessageType.Warning);
+            
+            var canLoad = (locationManager != null) && (location != null) &&
+                          ((locationManager.GlobalScenes.Count > 0) || (location.Scenes.Count > 0));  
+            GUI.enabled = canLoad;
+            if (GUILayout.Button("Load location")) LoadScene(locationManager, location);
+            GUI.enabled = true;
+        }
+
+        private void LoadScene(LocationManager locationManager, Location location)
+        {
+            var openScenes = Enumerable.Range(0, EditorSceneManager.loadedSceneCount)
+                .Select(EditorSceneManager.GetSceneAt)
+                .Where(scene => location.Scenes.All(x => scene.path != x.ScenePath))
+                .Where(scene => locationManager.GlobalScenes.All(x => scene.path != x.ScenePath))
+                .ToArray();
+            if (!EditorSceneManager.SaveModifiedScenesIfUserWantsTo(openScenes)) return;
+            foreach (var sceneAsset in location.Scenes)
+            {
+                EditorSceneManager.OpenScene(sceneAsset.ScenePath, OpenSceneMode.Additive);
+            }
+            foreach (var sceneAsset in locationManager.GlobalScenes)
+            {
+                EditorSceneManager.OpenScene(sceneAsset.ScenePath, OpenSceneMode.Additive);
+            }
+            EditorSceneManager.SetActiveScene(EditorSceneManager.GetSceneByPath(location.ActiveScene.ScenePath));
+            foreach (var openScene in openScenes)
+            {
+                EditorSceneManager.CloseScene(openScene, true);
+            }
+        }
+    }
+}
---
+++ Editor/LocationManagerEditor.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Games.GrumpyBear.LevelManagement.Editor
+{
+    [CustomEditor(typeof(LocationManager))]
+    public class LocationManagerEditor: UnityEditor.Editor
+    {
+        private LocationManager _locationManager;
+
+        private void OnEnable()
+        {
+            _locationManager = target as LocationManager;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var hasProblems = false;
+            foreach (var scene in _locationManager.GlobalScenes)
+            {
+                if (string.IsNullOrEmpty(scene.ScenePath)) continue;
+                if (scene.BuildIndex != -1) continue;
+                EditorGUILayout.HelpBox($"{scene.ScenePath} is missing from the build", MessageType.Warning);
+                hasProblems = true;
+            }
+            
+            if (hasProblems && GUILayout.Button("Fix all problems")) FixAllProblems();
+        }
+        
+        private void FixAllProblems()
+        {
+            var editorBuildSettingsScenes = EditorBuildSettings.scenes.ToList();
+            editorBuildSettingsScenes.AddRange(
+                from scene in _locationManager.GlobalScenes
+                where scene.BuildIndex == -1
+                select new EditorBuildSettingsScene(scene.ScenePath, true)
+            );
+            EditorBuildSettings.scenes = editorBuildSettingsScenes.ToArray();
+        }
+       
+    }
+}
