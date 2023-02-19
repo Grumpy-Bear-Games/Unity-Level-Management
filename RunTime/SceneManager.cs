9daf831e351072c88a26caba4954a57c70770028
Martin Parm <parmus@gmail.com>
2023-02-19 17:44:43 +0100

    Add support for waiting on SceneGroup loading to complete

--- RunTime/SceneManager.cs
+++ RunTime/SceneManager.cs
@@ -9,8 +9,16 @@ namespace Games.GrumpyBear.LevelManagement
     [CreateAssetMenu(menuName = "Grumpy Bear Games/Level Management/Scene Manager")]
     public class SceneManager: ScriptableObject
     {
-        public static event Action<SceneGroup> OnSceneGroupChanged;  
-            
+        public static event Action<SceneGroup> OnSceneGroupChanged;
+        public static SceneGroup SceneGroupCurrentlyLoading;
+        public static bool IsLoadingSceneGroup => SceneGroupCurrentlyLoading != null;
+
+        public static IEnumerator WaitForLoadingCompleted()
+        {
+            while (IsLoadingSceneGroup) yield return null;
+        }
+
+
         [SerializeField] private List<SceneReference> _globalScenes = new();
         [SerializeField] private List<SceneGroup> _sceneGroups = new();
         public IReadOnlyList<SceneReference> GlobalScenes => _globalScenes;
@@ -20,11 +28,22 @@ namespace Games.GrumpyBear.LevelManagement
 
         public IEnumerator Load_CO(SceneGroup sceneGroup)
         {
+            if (IsLoadingSceneGroup)
+            {
+                if (SceneGroupCurrentlyLoading != sceneGroup)
+                {
+                    Debug.LogError($"Failed to load SceneGroup {sceneGroup.name} while SceneGroup {SceneGroupCurrentlyLoading.name} is already being loaded");
+                }
+                yield break;
+            }
+
+            SceneGroupCurrentlyLoading = sceneGroup;
             var sceneReferencesToLoad = sceneGroup.Scenes.Concat(_globalScenes).Select(scene => scene.ScenePath);
             yield return SceneLoader.LoadExactlyByScenePath(sceneReferencesToLoad, sceneGroup.ActiveScene.ScenePath);
             OnSceneGroupChanged?.Invoke(sceneGroup);
+            SceneGroupCurrentlyLoading = null;
         }
-        
+
         #if UNITY_EDITOR
         public const string GlobalScenesPropertyName = nameof(_globalScenes);
         public const string SceneGroupsPropertyName = nameof(_sceneGroups);
